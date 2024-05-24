b51e5fa7e0f2e913ec2e93d010635031c72117df
bayareaunicorn <scottbeeker415@gmail.com>
2024-05-24 01:18:09 -0700

    social media icons svgs to functional components

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import {
+//     Facebook,
+//     X,
+//     Instagram,
+//     Linkedin,
+//     }
+//     from "../../assets/svgs";
 
 
 
 
-export default  footer => { return ( <h3>footer</h3>)}
+export default function Footer()  {  
+    return ( 
+        <div className="footer-container">
+            <div className="social-links">
+            <span>
+                <a href="https://facebook.com/hophippyfb">Facebook</a>
+            </span>
+            <span>
+                <a href="https://instagram.com/hophippyapp">Instagram</a>
+            </span>
+            <span>
+                <a href="https://linkedin.com/company/hophippy">Linkedin</a>
+            </span>
+            <span>
+                <a href="https://x.com/hophippyapp">X</a>
+            </span>
+            </div>
+            <div className="corp-links">
+                <span>
+                    <a href="https://hophippy.com/team">Team</a>
+                </span>
+                <span>
+                    <a href="https://hopippy.com/contact">Support</a>
+                </span>
+                <span>
+                    <a href="https://hophippy.com/investors">Investors</a>
+                </span>
+
+            </div>
+            <div className="info-links">
+                <span>
+                    <a href="https://hophippy.com/featuresover">Features</a>
+                </span>
+
+            </div>
+            <div className="creator-links">
+
+            </div>
+            
+            
+        </div>
+    )
+    
+}
